d07e34cebbd859a32f3687e00a59103bebd5550f
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-03 17:40:57 +0800

    v1.0.0-beta6
    fix a dumb bug

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ export const provider = {
 
 export const default_provider = provider.Nexra;
 
-export const generate = async (prompt = "", provider = default_provider, options = {}, {fetch = global.fetch} = {}) => {
-    if (provider === provider.Nexra) {
+export const generate = async (prompt = "", _provider = default_provider, options = {}, {fetch = global.fetch} = {}) => {
+    if (_provider === provider.Nexra) {
         return await getNexraResponse(prompt, options, {fetch: fetch});
     } else {
         throw new Error("Provider not found.");
