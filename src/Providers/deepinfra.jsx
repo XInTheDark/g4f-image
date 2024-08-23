9ed42a72cf4a81d4ed2e0c1b2a12419f5964ed0b
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-08-23 22:17:02 +0800

    Add DeepInfra

---
+++ src/Providers/deepinfra.jsx
@@ -0,0 +1,46 @@
+const api_url = "https://api.deepinfra.com/v1/inference";
+
+const headers = {
+    Accept: "text/event-stream",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US",
+    "Content-Type": "application/json",
+    Connection: "keep-alive",
+    Origin: "https://deepinfra.com",
+    Referer: "https://deepinfra.com/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "X-Deepinfra-Source": "web-embed",
+    "sec-ch-ua": '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+};
+
+export const getDeepInfraResponse = async (prompt, options, {fetch = global.fetch} = {}) => {
+    if (!(typeof fetch === "function")) {
+        throw new Error("Fetch is not defined globally. Please provide a polyfill.");
+    }
+
+    let model = options.model;
+    let url = `${api_url}/${model}`;
+
+    let data = {prompt: prompt};
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error(`status: ${response.status}, error: ${await response.text()}`);
+    }
+
+    let result = await response.json();
+    return result.images[0].split(';base64,').pop();
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import {getNexraResponse} from "./Providers/nexra";
+import {getDeepInfraResponse} from "./Providers/deepinfra";
 
 export const provider = {
     Nexra: "Nexra",
+    DeepInfra: "DeepInfra",
 };
 
 export const default_provider = provider.Nexra;
@@ -9,6 +11,8 @@ export const default_provider = provider.Nexra;
 export const generate = async (prompt = "", _provider = default_provider, options = {}, {fetch = global.fetch} = {}) => {
     if (_provider === provider.Nexra) {
         return await getNexraResponse(prompt, options, {fetch: fetch});
+    } else if (_provider === provider.DeepInfra) {
+        return await getDeepInfraResponse(prompt, options, {fetch: fetch});
     } else {
         throw new Error("Provider not found.");
     }
