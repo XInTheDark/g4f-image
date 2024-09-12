3e974ebf47f9f0abd1dda447070dc466bcaed452
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-09-12 11:53:59 +0800

    v1.2.0-beta.1

--- src/Providers/rocks.jsx
+++ src/Providers/rocks.jsx
@@ -1,21 +1,21 @@
 const api_url = "https://api.airforce/imagine";
 
 const headers = {
-  Accept: "application/json",
-  "Accept-Encoding": "gzip, deflate, br, zstd",
-  "Accept-Language": "en-US,en;q=0.9",
-  Authorization: "Bearer missing api key",
-  Origin: "https://llmplayground.net",
-  Referer: "https://llmplayground.net/",
-  "Sec-Fetch-Dest": "empty",
-  "Sec-Fetch-Mode": "cors",
-  "Sec-Fetch-Site": "same-origin",
-  "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-  "Content-Type": "application/json", // important as we are sending a JSON payload
+    Accept: "application/json",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "en-US,en;q=0.9",
+    Authorization: "Bearer missing api key",
+    Origin: "https://llmplayground.net",
+    Referer: "https://llmplayground.net/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "Content-Type": "application/json", // important as we are sending a JSON payload
 };
 
-export const getRocksResponse = async (prompt, options, {fetch = global.fetch} = {}) => {
+export const getRocksResponse = async (prompt, options, { fetch = global.fetch } = {}) => {
     if (!(typeof fetch === "function")) {
         throw new Error("Fetch is not defined globally. Please provide a polyfill.");
     }
@@ -37,13 +37,19 @@ export const getRocksResponse = async (prompt, options, {fetch = global.fetch} =
         throw new Error(`status: ${response.status}, error: ${await response.text()}`);
     }
 
-    const binaryImage = await response.blob();
-    // convert the binary image to base64
-    const reader = new FileReader();
-    reader.readAsDataURL(binaryImage);
-    return new Promise((resolve) => {
-        reader.onloadend = () => {
-            resolve(reader.result);
-        };
-    });
+    // get raw binary image data
+    const binaryImage = await (await response.blob()).arrayBuffer();
+
+    // convert to base64
+    function _arrayBufferToBase64(buffer) {
+        let binary = '';
+        const bytes = new Uint8Array(buffer);
+        const len = bytes.byteLength;
+        for (let i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
+    return _arrayBufferToBase64(binaryImage);
 }
