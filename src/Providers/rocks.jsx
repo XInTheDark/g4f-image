c4426836ade32d1248fe69c272638ec6b1ad5d47
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-09-12 11:56:46 +0800

    v1.2.0-beta.2

--- src/Providers/rocks.jsx
+++ src/Providers/rocks.jsx
@@ -42,13 +42,17 @@ export const getRocksResponse = async (prompt, options, { fetch = global.fetch }
 
     // convert to base64
     function _arrayBufferToBase64(buffer) {
-        let binary = '';
-        const bytes = new Uint8Array(buffer);
-        const len = bytes.byteLength;
-        for (let i = 0; i < len; i++) {
-            binary += String.fromCharCode(bytes[i]);
+        if (Buffer && Buffer.from) {
+            return Buffer.from(buffer).toString('base64');
+        } else {
+            let binary = '';
+            const bytes = new Uint8Array(buffer);
+            const len = bytes.byteLength;
+            for (let i = 0; i < len; i++) {
+                binary += String.fromCharCode(bytes[i]);
+            }
+            return window.btoa(binary);
         }
-        return window.btoa(binary);
     }
 
     return _arrayBufferToBase64(binaryImage);
